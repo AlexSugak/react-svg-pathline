ba3787fd11af784c30ef9bc47724b14b86eb8de0
Matthieu Lemoine <mlemoine@yumii.fr>
2017-04-27 14:45:50 +0200

    :alien: Remove deprecated React API

--- index.jsx
+++ index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 const isCollinear = (p1, p2, p3) => {
     return (p1.y - p2.y) * (p1.x - p3.x) == (p1.y - p3.y) * (p1.x - p2.x);
@@ -11,42 +12,40 @@ const moveTo = (b, a, r) => {
     return {x: a.x + unitVector.x * r, y: a.y + unitVector.y * r};
 }
 
-export const PathLine = React.createClass({
-    render: function(){
-        let {points, r, ...other} = this.props;
-        const path = points
-                        .slice(1)
-                        .reduce((acc, p, i, points) => { 
-                                    let next = points[i + 1];
-                                    let prev = acc[acc.length - 1];
-                                    
-                                    if (next && !isCollinear(prev.point, p, next)) {
-                                        let before = moveTo(prev.point, p, r);
-                                        let after = moveTo(next, p, r);
-                                        return acc.concat({
-                                            point:p,
-                                            s:`L ${before.x} ${before.y} S ${p.x} ${p.y} ${after.x} ${after.y} `
-                                        })
-                                    } else { 
-                                        return acc.concat({
-                                            point:p,
-                                            s:`L ${p.x} ${p.y} `
-                                        })
-                                    };
-                                }
-                                , [{
-                                    point: this.props.points[0], 
-                                    s: `M ${this.props.points[0].x} ${this.props.points[0].y} `
-                                }])
-                        .map(p => p.s)
-                        .join();
-        return (
-            <path d={path} {...other} />
-        )
-    }
-})
+export const PathLine = () => {
+    let {points, r, ...other} = this.props;
+    const path = points
+                    .slice(1)
+                    .reduce((acc, p, i, points) => {
+                                let next = points[i + 1];
+                                let prev = acc[acc.length - 1];
 
-PathLine.propTypes = { 
-  points: React.PropTypes.array.isRequired, 
-  r: React.PropTypes.number.isRequired
+                                if (next && !isCollinear(prev.point, p, next)) {
+                                    let before = moveTo(prev.point, p, r);
+                                    let after = moveTo(next, p, r);
+                                    return acc.concat({
+                                        point:p,
+                                        s:`L ${before.x} ${before.y} S ${p.x} ${p.y} ${after.x} ${after.y} `
+                                    })
+                                } else {
+                                    return acc.concat({
+                                        point:p,
+                                        s:`L ${p.x} ${p.y} `
+                                    })
+                                };
+                            }
+                            , [{
+                                point: this.props.points[0],
+                                s: `M ${this.props.points[0].x} ${this.props.points[0].y} `
+                            }])
+                    .map(p => p.s)
+                    .join();
+    return (
+        <path d={path} {...other} />
+    )
+};
+
+PathLine.propTypes = {
+  points: PropTypes.array.isRequired,
+  r: PropTypes.number.isRequired
 }
