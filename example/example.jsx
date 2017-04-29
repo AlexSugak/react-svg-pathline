8c62b7b70a3e90a4fe946c0390d326a75ed07e8a
Александр <aleksandr.sugak@gmail.com>
2017-04-29 20:20:42 +0300

    update peer dependencies and make example pure function

--- example/example.jsx
+++ example/example.jsx
@@ -2,31 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {PathLine} from '../index'
 
-export const App = React.createClass({
-    render: function(){
-        return (
-            <div style={{paddingLeft:'100px'}}>
-                <svg width="300" height="300" style={{paddingTop: '10px'}}>
-                    <polyline 
-                        points="0,0 125,0 125,125 250,125" 
-                        stroke="red" 
-                        strokeWidth="3"
-                        fill="none" />
-                </svg>
-                
-                <svg width="300" height="300" style={{paddingTop: '10px'}}>
-                    <PathLine 
-                        points={[{x:0, y:0}, {x:125, y: 0}, {x:125, y:125}, {x:250, y:125}]} 
-                        stroke="red" 
-                        strokeWidth="3"
-                        fill="none"
-                        r={10}
-                        />
-                </svg>
-            </div>
-        )
-    }
-})
+export const App = () => (
+    <div style={{paddingLeft:'100px'}}>
+        <svg width="300" height="300" style={{paddingTop: '10px'}}>
+            <polyline 
+                points="0,0 125,0 125,125 250,125" 
+                stroke="red" 
+                strokeWidth="3"
+                fill="none" />
+        </svg>
+        
+        <svg width="300" height="300" style={{paddingTop: '10px'}}>
+            <PathLine 
+                points={[{x:0, y:0}, {x:125, y: 0}, {x:125, y:125}, {x:250, y:125}]} 
+                stroke="red" 
+                strokeWidth="3"
+                fill="none"
+                r={10}
+                />
+        </svg>
+    </div>
+);
 
 ReactDOM.render(
   <App>
